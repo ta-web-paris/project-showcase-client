7c7ce51c380cbddc9e1a479075429db3e12c5ba8
abisummers <hello@abisummers.com>
2018-11-26 09:48:12 +0100

    Header homepage (#1)
    * imported HeaderHome component
    
    * log out function added
    
    * removed console.log

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
+
 import "./App.css";
 import api from "./api.js";
 import Home from "./components/HomePage/Home";
@@ -44,14 +45,13 @@ class App extends Component {
   }
 
   userLoggedIn = user => {
-    console.log("New USER\n", user);
     this.setState({
       isLoginChecked: true,
       currentUser: user
     });
   };
 
-  logUserOut = () => {
+  logUserOut = event => {
     api
       .delete("/logout")
       .then(response => {
@@ -71,6 +71,35 @@ class App extends Component {
         {/* index name is what i called the data on algolia */}
         <InstantSearch searchClient={searchClient} indexName="dev_data">
           <HeaderHome currentUser={currentUser} />
+          <HeaderHome
+            currentUser={currentUser}
+            logUserOut={event => this.logUserOut(event)}
+          />
+
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route
+              path="/login"
+              render={() =>
+                currentUser ? (
+                  <Redirect to="/" />
+                ) : (
+                  <Login handleLogIn={this.userLoggedIn} />
+                )
+              }
+            />
+            <Route
+              path="/settings"
+              render={() =>
+                isLoginChecked && !currentUser ? (
+                  <Redirect to="/login" />
+                ) : (
+                  <SettingsPage currentUser={currentUser} />
+                )
+              }
+            />
+            <Route component={ErrorPage} />
+          </Switch>
 
           <Switch>
             <Route exact path="/" component={Home} />
--- src/components/HomePage/HeaderHome.js
+++ src/components/HomePage/HeaderHome.js
@@ -4,11 +4,18 @@ import SearchBar from "../SearchBar";
 
 class HomeHeader extends Component {
   state = {};
+
+  logUserOut(event) {
+    console.log("gogoggogogo", this.props);
+    this.setState({ isLoginChecked: false });
+  }
+
   render() {
-    const { currentUser } = this.props;
+    const { currentUser, logUserOut } = this.props;
     return (
       <div>
         <header className="App-header">
+          <h1>This is our Project Showcase app!</h1>
           <NavLink exact to="/">
             Home
           </NavLink>
@@ -17,6 +24,7 @@ class HomeHeader extends Component {
               <NavLink to="/settings">User Settings</NavLink>
               <b>{currentUser.email}</b>
               <button onClick={this.logUserOut}>Log out</button>
+              <button onClick={event => logUserOut(event)}>Log out</button>
             </React.Fragment>
           ) : (
             <NavLink to="/login">Log in</NavLink>
