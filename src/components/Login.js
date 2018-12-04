a98a65dc015fae020e18aa7b219b07608df2c2d4
abisummers <hello@abisummers.com>
2018-12-04 21:55:24 +0100

    fix: added classname to form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,49 +1,57 @@
-import React from 'react';
-import api from '../api.js';
-
+import React from "react";
+import api from "../api.js";
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
 
-    constructor(props){
-        super(props)
-        this.state = {
-            email: '',
-            password: ''
-        }
-    }
+  handleChange = event => {
+    let myInput = event.target;
+    let name = myInput.id;
+    this.setState({
+      [name]: myInput.value
+    });
+  };
 
-    handleChange = (event) => {
-        let myInput = event.target;
-        let name = myInput.id;
-        this.setState({
-            [name]: myInput.value
-        });
-    }
+  handleSubmit = event => {
+    event.preventDefault();
+    api
+      .post("/login", this.state)
+      .then(response => {
+        const { userDoc } = response.data;
+        this.props.handleLogIn(userDoc);
+      })
+      .catch(err => console.log(err));
+  };
 
-    handleSubmit = (event) => {
-        event.preventDefault();
-        api.post('/login', this.state)
-          .then(response=>{
-            const {userDoc} = response.data;
-            this.props.handleLogIn(userDoc);
-          })
-          .catch(err => console.log(err))
-    }
+  render() {
+    return (
+      <form className="loginForm" onSubmit={this.handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          onChange={this.handleChange}
+          id="email"
+          type="email"
+          placeholder="Your email here"
+        />
 
-    render() {
-        return(
-            <form onSubmit={ this.handleSubmit }>
-                <label htmlFor="email">Email</label>
-                <input onChange={ this.handleChange } id="email" type="email" placeholder="Your email here" />
+        <label htmlFor="password">Password</label>
+        <input
+          onChange={this.handleChange}
+          id="password"
+          type="password"
+          placeholder="We won't tell"
+        />
 
-                <label htmlFor="password">Password</label>
-                <input onChange={ this.handleChange } id="password" type="password" placeholder="We won't tell" />
-
-                <button>Submit</button>
-            </form>
-        )
-    }
+        <button>Submit</button>
+      </form>
+    );
+  }
 }
-
 
 export default Login;
