d5372e77717dae741e43bf6826424257f1a165ed
abisummers <hello@abisummers.com>
2019-03-18 12:44:04 +0100

    display project suggestions

--- src/components/ProjectPage/ProjectSuggestion.js
+++ src/components/ProjectPage/ProjectSuggestion.js
@@ -1,42 +1,62 @@
 import React, { Component } from "react";
-// import axios from "axios";
-// import $ from "jquery";
-
+import axios from "axios";
+// import { Link } from "react-router-dom";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import "./style/ProjectSuggestion.scss";
+import { Link } from "@reach/router";
 
 class ProjectSuggestion extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      ProjectSuggest: {}
+      projectInfo: [
+        {
+          name: "",
+          projectType: "",
+          squad: "",
+          description: "",
+          gitHubUrl: " ",
+          projectUrl: "",
+          github: "",
+          linkedin: "",
+          screenshotUrl: "",
+          likes: "",
+          tools: [],
+          creators: [],
+          projectCredentials: []
+        }
+      ]
     };
   }
 
+  componentDidMount() {
+    const { params } = this.props.match;
+
+    // console.log(this.props.match);
+    console.log(params);
+
+    axios
+      .get(`http://localhost:4000/api/creator-projects/${params.projectId}`, {
+        withCredentials: true
+      })
+      .then(response => {
+        this.setState({
+          projectInfo: response.data
+        });
+      })
+      .catch(err => {
+        console.log("Project Details ERROR", err);
+        alert("Sorry! Something went wrong.");
+      });
+  }
+
   render() {
-    console.log(this.props);
-    // const { projectSuggest } = this.props;
-    // const { projects } = this.state;
-    // const {
-    //   name,
-    //   creators,
-    //   screenshotUrl,
-    //   description,
-    //   gitHubUrl,
-    //   projectUrl,
-    //   projectType,
-    //   tools,
-    //   likes,
-    //   projectCredentials,
-    //   display,
-    //   bootcamp,
-    //   squad
-    // } = this.props;
+    console.log(this.state.projectInfo, "project info");
 
     return (
       <section id="ProjectSuggestion">
-        <h2 className="h2">Projects from X</h2>
+        <h2 className="h2">Projects from</h2>
 
         <div className="container">
           <div className="row blog">
@@ -49,42 +69,17 @@ class ProjectSuggestion extends Component {
                 <div className="carousel-inner">
                   <div className="carousel-item active">
                     <div className="row row3">
-                      <div className="col-lg-3 col-md-6">
-                        <a href="0">
-                          <img
-                            src="http://placehold.it/250x250"
-                            alt="placehold"
-                          />
-                        </a>
-                      </div>
-                      <div className="col-lg-3 col-md-6">
-                        <a href="0">
-                          <img
-                            src="http://placehold.it/250x250"
-                            alt="placehold"
-                          />
-                        </a>
-                      </div>
-                      <div className="col-lg-3 col-md-6">
-                        <a href="0">
-                          <img
-                            src="http://placehold.it/250x250"
-                            alt="placehold"
-                          />
-                        </a>
-                      </div>
-                      <div className="col-lg-3 col-md-6">
-                        <a href="0">
-                          <img
-                            src="http://placehold.it/250x250"
-                            alt="placehold"
-                          />
-                        </a>
-                      </div>
+                      {this.state.projectInfo.map(el => (
+                        <div key={el._id} className="col-lg-3 col-md-6">
+                          <a href={el.searchId}>
+                            <img src={el.screenshotUrl} alt="screenshot url" />
+                          </a>
+                        </div>
+                      ))}
                     </div>
                   </div>
 
-                  <div className="carousel-item">
+                  {/* <div className="carousel-item">
                     <div className="row row3">
                       <div className="col-lg-3 col-md-6">
                         <a href="0">
@@ -119,9 +114,9 @@ class ProjectSuggestion extends Component {
                         </a>
                       </div>
                     </div>
-                  </div>
+                  </div> */}
                 </div>
-                <ol className="carousel-indicators">
+                {/* <ol className="carousel-indicators">
                   <li
                     data-target="#blogCarousel"
                     data-slide-to="0"
@@ -132,7 +127,7 @@ class ProjectSuggestion extends Component {
                     data-slide-to="1"
                     className="indic"
                   />
-                </ol>
+                </ol> */}
               </div>
             </div>
           </div>
