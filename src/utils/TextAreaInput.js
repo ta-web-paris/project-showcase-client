6ce8fd54cb0c37a9c4729d233996f6d06994a6f7
abisummers <hello@abisummers.com>
2019-04-30 10:19:34 +0200

    changed class to className

--- src/utils/TextAreaInput.js
+++ src/utils/TextAreaInput.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
 const TextAreaInput = (props) => {
-  const {label, type = "text", name, onChange, value} = props;
+  const { label, type = "text", name, onChange, value } = props;
   return (
-      <label>{label}:
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"
-          type={type}
-          name={name}
-          onChange={onChange}
-          value={value}></textarea>
-        
-      </label>
-   )
+    <label>{label}:
+        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"
+        type={type}
+        name={name}
+        onChange={onChange}
+        value={value}></textarea>
+
+    </label>
+  )
 }
- 
+
 export default TextAreaInput;
