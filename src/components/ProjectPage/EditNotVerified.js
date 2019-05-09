034f751762da9b0cfabaa4d82fbb0bf4359871e2
abisummers <hello@abisummers.com>
2019-05-09 09:52:25 +0200

    fix: verify button only for nonverified projects

--- src/components/ProjectPage/EditNotVerified.js
+++ src/components/ProjectPage/EditNotVerified.js
@@ -3,15 +3,16 @@ import { Link } from "react-router-dom"
 class EditNotVerified extends Component {
   state = {}
   render() {
-    console.log(this.props);
-
-    const { searchId } = this.props
+    const { searchId, verified } = this.props.projectInfo
 
     return (<div>
       <Link to={`/projects/edit/${searchId}`}>
         <button className="btn btn-primary">Edit</button>
       </Link>
 
+      {verified ? "" : <button className="btn btn-primary">Verify</button>}
+
+      <button className="btn btn-primary">Delete</button>
     </div >
     );
   }
